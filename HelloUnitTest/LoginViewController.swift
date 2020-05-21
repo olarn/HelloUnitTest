6539c1b17374b4c2566817f0ebe3d09dc341f895
Olarn U <olarn.u@gmail.com>
2020-05-21 09:52:08 +0700

    Complete Login UI

--- HelloUnitTest/LoginViewController.swift
+++ HelloUnitTest/LoginViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+    }
 }
 
