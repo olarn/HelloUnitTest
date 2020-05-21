fe8570813418ffcc921e27d0b3a60536abcc1c99
Olarn U <olarn.u@gmail.com>
2020-05-21 09:36:08 +0700

    Initial Commit

---
+++ HelloUnitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloUnitTest
+//
+//  Created by Olarn U. on 21/5/2563 BE.
+//  Copyright © 2563 Olarn U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloUnitTestTests/HelloUnitTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  HelloUnitTestTests.swift
+//  HelloUnitTestTests
+//
+//  Created by Olarn U. on 21/5/2563 BE.
+//  Copyright © 2563 Olarn U. All rights reserved.
+//
+
+import XCTest
+@testable import HelloUnitTest
+
+class HelloUnitTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
